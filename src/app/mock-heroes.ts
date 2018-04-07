f66d2c0ba1d5f9f31699582c5edfc8af0561c5de
Martin Zembaty <m.zembaty@yahoo.de>
2018-04-07 09:45:10 +0200

    heros list

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Mr. Nice'},
+    {id: 12, name: 'Fluffy Pants'},
+    {id: 13, name: 'Power Puff'},
+    {id: 14, name: 'Sumo the Ringer'},
+    {id: 15, name: 'Ghostery'}
+]
